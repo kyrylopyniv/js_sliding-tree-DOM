31afdb2ffd2212909ea682000cb498350e1796c3
Kyrylo Pyniv <pynivkm@gmail.com>
2022-11-18 19:00:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+/* 1.перебираємо список li і додаєм туди спани на кожну лішку
+2.Кожному спану в лішці присвоюємо лістенер для приховування при кліку */
+
+for (const li of document.querySelectorAll('li')) {
+  const span = document.createElement('span');
+  const text = (li.firstChild);
+
+  li.prepend(span);
+  span.prepend(text);
+};
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', ev => {
+  const item = ev.target.closest('span');
+  const currList = ev.target.parentElement.children[1];
+
+  if (!item || !tree.contains(item)) {
+    return;
+  }
+
+  if (currList) {
+    currList.hidden = !currList.hidden;
+  }
+});
